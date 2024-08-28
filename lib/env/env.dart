2b5dbaa7b9bef802c64bdc262e05b1a3f08016c2
andhikangurah <andhikangurah@gmail.com>
2024-08-28 14:17:54 +0000

    add firebase setup

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -4,6 +4,20 @@ part 'env.g.dart';
 
 @Envied(path: '.env')
 abstract class Env {
-    @EnviedField(varName: 'FIREBASE_API_KEY')
-    static const String firebaseAPIKey = _Env.firebaseAPIKey;
+    @EnviedField(varName: 'FIREBASE_WEB_API_KEY')
+    static const String firebaseWebAPIKey = _Env.firebaseWebAPIKey;
+    @EnviedField(varName: 'FIREBASE_ANDROID_API_KEY')
+    static const String firebaseAndroidAPIKey = _Env.firebaseAndroidAPIKey;
+    @EnviedField(varName: 'FIREBASE_AUTH_DOMAIN')
+    static const String firebaseAuthDomain = _Env.firebaseAuthDomain;
+    @EnviedField(varName: 'FIREBASE_PROJECT_ID')
+    static const String firebaseProjectID = _Env.firebaseProjectID;
+    @EnviedField(varName: 'FIREBASE_STORAGE_BUCKET')
+    static const String firebaseStorageBucket = _Env.firebaseStorageBucket;
+    @EnviedField(varName: 'FIREBASE_MESSAGING_SENDER_ID')
+    static const String firebaseMessagingSenderID = _Env.firebaseMessagingSenderID;
+    @EnviedField(varName: 'FIREBASE_WEB_APP_ID')
+    static const String firebaseWebAppID = _Env.firebaseWebAppID;
+    @EnviedField(varName: 'FIREBASE_ANDROID_APP_ID')
+    static const String firebaseAndroidAppID = _Env.firebaseAndroidAppID;
 }
