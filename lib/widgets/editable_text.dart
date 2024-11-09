3215d3bc22003b31a044bb92d5a4112b7452cb25
andhikangurah <andhikangurah@gmail.com>
2024-11-09 14:03:34 +0000

    add option to add custom input text styles in EditableText widget

--- lib/widgets/editable_text.dart
+++ lib/widgets/editable_text.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 class EditableText extends StatefulWidget {
-  const EditableText({super.key, this.keyboardType = TextInputType.text, this.validator, this.controller});
+  const EditableText({super.key, this.keyboardType = TextInputType.text, this.validator, this.controller, this.style});
 
   final TextInputType keyboardType;
   final FormFieldValidator? validator;
   final TextEditingController? controller;
+  final TextStyle? style;
 
   @override
   State<EditableText> createState() => EditableTextState();
@@ -21,6 +22,7 @@ class EditableTextState extends State<EditableText> {
       textAlign: TextAlign.center,
       cursorColor: Colors.grey,
       cursorWidth: 1, // Adjust this later
+      style: widget.style,
       decoration: const InputDecoration(
         border: InputBorder.none,    
         fillColor: Colors.transparent,
