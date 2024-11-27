acb298b372c68990b5ba76b688ff929450fdb48a
andhikangurah <andhikangurah@gmail.com>
2024-11-27 15:05:52 +0000

    add currency condition on editable text widget

--- lib/widgets/editable_text.dart
+++ lib/widgets/editable_text.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
 class PernanceEditableText extends StatefulWidget {
-  const PernanceEditableText({super.key, this.keyboardType = TextInputType.text, this.validator, this.controller, this.style, this.placeholder});
+  const PernanceEditableText({
+    super.key,
+    this.keyboardType = TextInputType.text,
+    this.validator,
+    this.controller,
+    this.style,
+    this.placeholder,
+    this.isCurrency = false
+  });
 
   final TextInputType keyboardType;
   final FormFieldValidator? validator;
   final TextEditingController? controller;
   final TextStyle? style;
   final String? placeholder;
+  final bool isCurrency;
 
   @override
   State<PernanceEditableText> createState() => EditableTextState();
@@ -16,7 +25,22 @@ class PernanceEditableText extends StatefulWidget {
 class EditableTextState extends State<PernanceEditableText> {
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
+    return widget.isCurrency ? TextFormField(
+      keyboardType: widget.keyboardType,
+      validator: widget.validator,
+      controller: widget.controller,
+      textAlign: TextAlign.center,
+      cursorColor: Colors.grey,
+      cursorWidth: 1, // Adjust this later
+      style: widget.style,
+      decoration: InputDecoration(
+        border: InputBorder.none,    
+        fillColor: Colors.transparent,
+        filled: true,
+        hintText: widget.placeholder,
+        hintStyle: const TextStyle(color: Colors.black12),
+      ),
+    ) : TextFormField(
       keyboardType: widget.keyboardType,
       validator: widget.validator,
       controller: widget.controller,
