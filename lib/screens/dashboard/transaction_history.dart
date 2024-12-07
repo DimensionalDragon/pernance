2a6476b130d9bc9c3f101e1c5c9dabd356b3cc46
andhikangurah <andhikangurah@gmail.com>
2024-12-07 14:22:05 +0000

    add transaction history screen

---
+++ lib/screens/dashboard/transaction_history.dart
@@ -0,0 +1,25 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class TransactionHistoryScreen extends StatelessWidget {
+  const TransactionHistoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Text('Transaction History'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
